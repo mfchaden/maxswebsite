a505b404c574f52dc2035813ae99437a71927cbc
mfchaden <maxhaden52@gmail.com>
2020-04-10 19:13:28 +0200

    url update

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from max import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('max/', include('max.urls'))
+    path('', views.index, name='index'),
 ]
